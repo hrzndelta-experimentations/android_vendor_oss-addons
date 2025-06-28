0419d97d9af5cc443d03aad30e6117444bcaf727
hrzndelta <widiadinata1130@proton.me>
2025-06-28 18:06:02 +0800

    adding packages to oss-addons.mk

--- oss-addons.mk
+++ oss-addons.mk
@@ -1,10 +1,9 @@
 # Include package in this vendor
 PRODUCT_PACKAGE += \
-	AuroraStore \
-	Droid-ify \
-	FakeStore \
-	GMSCore \
-	GSFProxy
-
-# Include package from GMS vendor
-PRODUCT_PACKAGE += \
+	BreezyWeather \
+	Fennec \
+	Gramophone \
+	HeliBoard \
+	ProtonVPN \
+	Shizuku \
+	Smartspacer
